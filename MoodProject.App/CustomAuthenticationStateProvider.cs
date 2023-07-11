6d5f3d16e2a4c4708445a741346c2ad6c7c3ffa7
DownoBE <donovanhoyois@hotmail.com>
2023-07-11 16:02:57 +0200

    login and logout with auth0

---
+++ MoodProject.App/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,23 @@
+﻿/*
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace MoodProject.App;
+
+public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var identity = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.Name, "test"),
+        }, "Custom Authentication");
+        
+        
+
+        var user = new ClaimsPrincipal(identity);
+
+        return Task.FromResult(new AuthenticationState(user));
+    }
+}
+*/
--- MoodProject.App/Program.cs
+++ MoodProject.App/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MoodProject.App;
@@ -11,6 +12,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44337/") });
 
 // Custom
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+    //options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]);
+});
+//builder.Services.AddSingleton<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddSingleton<ISymptomsTypesService, SymptomsTypesService>();
 builder.Services.AddSingleton<ISymptomsFormService, SymptomsFormService>();
 
