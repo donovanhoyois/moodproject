528c02aeed7b343a13d765298e5eb691cdc37d45
DownoBE <donovanhoyois@hotmail.com>
2023-11-16 17:23:58 +0100

    cache popup when new version is available

---
+++ MoodProject.App/Configuration/CacheConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace MoodProject.App.Configuration;
+
+public class CacheConfiguration
+{
+    public string Version { get; set; }
+}
--- MoodProject.App/wwwroot/service-worker.published.js
+++ MoodProject.App/wwwroot/service-worker.published.js
@@ -28,6 +28,7 @@ async function onInstall(event) {
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
         .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    skipWaiting();
 }
 
 async function onActivate(event) {
