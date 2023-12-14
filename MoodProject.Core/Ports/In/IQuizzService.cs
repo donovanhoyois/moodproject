4f74850a1d56fb477af5b852c8ef1ee44a4cb669
DownoBE <donovanhoyois@hotmail.com>
2023-12-14 21:42:05 +0100

    Revert "wip"
    This reverts commit d8011b8876f77a8a060438084835db1fea5512d0.

--- MoodProject.Core/Ports/In/IQuizzService.cs
+++ MoodProject.Core/Ports/In/IQuizzService.cs
@@ -7,8 +7,8 @@ public interface IQuizzService
 {
     public Task<OperationResult<IEnumerable<QuizzQuestion>>> Generate(string userId);
     public Task<bool> Submit(IEnumerable<Symptom> symptoms, IEnumerable<FactorValue> values);
-    public decimal GetAverageValues(Symptom symptom, int max, int offset = 0, FactorType? type = null);
-    public decimal? GetHealthAverage(IEnumerable<Symptom> symptoms, int offset = 0);
-    public IEnumerable<decimal?> GetHealthAverageHistory(IEnumerable<Symptom> symptoms, int depth);
-    public OperationResult<decimal?> GetHealthAverageAsPercentage(IEnumerable<Symptom> symptoms, int offset = 0);
+    public float GetAverageValues(Symptom symptom, int max, int offset = 0, FactorType? type = null);
+    public float GetHealthAverage(IEnumerable<Symptom> symptoms, int offset = 0);
+    public IEnumerable<float> GetHealthAverageHistory(IEnumerable<Symptom> symptoms, int depth);
+    public OperationResult<float> GetHealthAverageAsPercentage(IEnumerable<Symptom> symptoms, int offset = 0);
 }
