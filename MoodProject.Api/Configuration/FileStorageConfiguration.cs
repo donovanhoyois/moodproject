d42a07e0530effebca98fd3290062cd33c876113
DownoBE <donovanhoyois@hotmail.com>
2023-12-26 15:28:10 +0100

    connection to azure blob storage working

---
+++ MoodProject.Api/Configuration/FileStorageConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace MoodProject.Api.Configuration;
+
+public class FileStorageConfiguration
+{
+    public string AccountName { get; init; }
+    public string AccountKey { get; init; }
+}
---
+++ MoodProject.Api/Controllers/FilesController.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MoodProject.Api.Configuration;
+
+namespace MoodProject.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]/")]
+[EnableCors]
+public class FilesController
+{
+    private BlobServiceClient BlobServiceClient { get; set; }
+    
+    public FilesController(FileStorageConfiguration fileStorageConfiguration)
+    {
+        BlobServiceClient = new BlobServiceClient(
+            new Uri("https://moodprojectstorage.blob.core.windows.net"),
+            new StorageSharedKeyCredential(fileStorageConfiguration.AccountName, fileStorageConfiguration.AccountKey));
+    }
+
+    [HttpGet, ActionName("CheckConnection")]
+    public async Task<bool> CheckConnection()
+    {
+        await BlobServiceClient.CreateBlobContainerAsync("test");
+        return true;
+    }
+}
