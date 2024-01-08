c4f0c210386c5eb680b140f72d1bfa18433da361
DownoBE <donovanhoyois@hotmail.com>
2024-01-08 13:58:32 +0100

    migrations

---
+++ MoodProject.Api/Migrations/20231213204013_NotificationSubscriptions_renamed_id.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoodProject.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotificationSubscriptions_renamed_id : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NotificationSubscriptionId",
+                table: "NotificationSubscriptions",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "NotificationSubscriptions",
+                newName: "NotificationSubscriptionId");
+        }
+    }
+}
---
+++ MoodProject.Api/Migrations/20231226113951_MedicationDayUsage_utcOffset.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoodProject.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MedicationDayUsage_utcOffset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UtcOffset",
+                table: "MedicationDayUsages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UtcOffset",
+                table: "MedicationDayUsages");
+        }
+    }
+}
