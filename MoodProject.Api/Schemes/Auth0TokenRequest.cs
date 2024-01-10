fc27e5384e645daec27490aac17c56714d4ab33a
DownoBE <donovanhoyois@hotmail.com>
2024-01-10 18:52:26 +0100

    Auth0TokenRequest

---
+++ MoodProject.Api/Schemes/Auth0TokenRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MoodProject.Api.Schemes;
+
+public class Auth0TokenRequest
+{
+    [JsonPropertyName("client_id")]
+    public string ClientId { get; set; }
+    [JsonPropertyName("client_secret")]
+    public string ClientSecret { get; set; }
+    [JsonPropertyName("audience")]
+    public string Audience { get; set; }
+    [JsonPropertyName("grant_type")]
+    public string GrantType { get; set; }
+}
